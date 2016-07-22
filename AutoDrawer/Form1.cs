2ad1a3bfd3fd95428a004e963d9d187ea85f7aa8
rainzhao2000 <rainzhao2000@gmail.com>
2016-07-22 01:10:56 -0400

    added drawing pattern picture to documentaion

--- AutoDrawer/Form1.cs
+++ AutoDrawer/Form1.cs
@@ -529,6 +529,18 @@ new float[] {0, 0, 0, 0, 1}
 
             }
         }
+
+        public static void Print2DArray(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 
     class Position
